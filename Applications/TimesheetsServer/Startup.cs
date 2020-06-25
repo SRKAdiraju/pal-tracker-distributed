ba66f9b6ee9c2567a9839212ca7622824b859070
srkadiraju <srk.adiraju1996@gmail.com>
2020-06-25 13:52:40 +0530

    Circuit Breaker 1.0

--- Applications/TimesheetsServer/Startup.cs
+++ Applications/TimesheetsServer/Startup.cs
@@ -11,6 +11,8 @@ using Timesheets.Data;
 using Timesheets.ProjectClient;
 using Steeltoe.Discovery.Client;
 using Steeltoe.Common.Discovery;
+ using Microsoft.Extensions.Logging;
+ using Steeltoe.CircuitBreaker.Hystrix;
 
 namespace TimesheetsServer
 {
@@ -41,8 +43,11 @@ namespace TimesheetsServer
                     BaseAddress = new Uri(Configuration.GetValue<string>("REGISTRATION_SERVER_ENDPOINT"))
                 };
 
-                return new ProjectClient(httpClient);
-            });
+                 var logger = sp.GetService<ILogger<ProjectClient>>();
+                 return new ProjectClient(httpClient, logger);
+              });
+
+             services.AddHystrixMetricsStream(Configuration);
 
         }
 
@@ -56,6 +61,8 @@ namespace TimesheetsServer
 
             app.UseCloudFoundryActuators();
             app.UseDiscoveryClient();
+           app.UseHystrixMetricsStream();
+           app.UseHystrixRequestContext();
             app.UseRouting();
 
             app.UseAuthorization();
